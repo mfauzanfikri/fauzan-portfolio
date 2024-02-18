cf6e5326b23e81ab8f22b39e250ecfc803e38f8a
mfauzanfikri <mfauzanfikri09@gmail.com>
2024-02-18 12:01:12 +0700

    feat: update button element in contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -21,7 +21,7 @@ const Contact = () => {
             </div>
             <div className="flex w-1/2 items-center justify-end lg:w-[20%]">
               <a href="mailto:mfauzanfikri09@gmail.com">
-                <button className="rounded-lg bg-primary px-3 py-1.5 text-sm text-white md:text-base">
+                <button className="rounded-lg bg-primary px-3 py-1.5 text-sm text-white hover:opacity-80 md:text-base">
                   Email Me
                 </button>
               </a>
@@ -34,7 +34,7 @@ const Contact = () => {
             </div>
             <div className="flex w-1/2 items-center justify-end lg:w-[20%]">
               <a href="mailto:mfauzanfikri09@gmail.com">
-                <button className="rounded-lg bg-primary px-3 py-1.5 text-sm text-white md:text-base">
+                <button className="rounded-lg bg-primary px-3 py-1.5 text-sm text-white hover:opacity-80 md:text-base">
                   Chat Me
                 </button>
               </a>
