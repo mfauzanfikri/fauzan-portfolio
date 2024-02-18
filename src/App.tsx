6818b87e36498c5ecee864b2affa231abf346940
mfauzanfikri <mfauzanfikri09@gmail.com>
2024-02-18 12:30:58 +0700

    feat: add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import About from "./components/about";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
@@ -5,10 +6,24 @@ import Header from "./components/header";
 import Hero from "./components/hero";
 import Portfolio from "./components/portfolio";
 import TechStack from "./components/techStack";
+import Loader from "./components/loader";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body?.classList.add("overflow-hidden");
+    setTimeout(() => {
+      body?.classList.remove("overflow-hidden");
+      setIsLoading(false);
+    }, 1200);
+  }, []);
+
   return (
     <>
+      {isLoading && <Loader />}
+
       <Header />
       <main>
         <Hero />
